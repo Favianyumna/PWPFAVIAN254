7aa2238f0f1f06021869442ee64a4c80de5b9ed4
PWPFAVIAN254 <favianyumna07@gmail.com>
2024-12-29 15:13:51 +0700

    setelah perbaikan dan mencari bug

--- app/models.py
+++ app/models.py
@@ -1,17 +1,9 @@
-from datetime import datetime
-from app import db, login_manager
-from flask_login import UserMixin
+from flask_sqlalchemy import SQLAlchemy
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
+db = SQLAlchemy()
 
-class User(db.Model, UserMixin):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(60), nullable=False)
-    date_registered = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-
-    def __repr__(self):
-        return f"User('{self.username}', '{self.email}')"
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    email = db.Column(db.String(110), unique=True, nullable=False)
+    password_hash = db.Column(db.String(200), nullable=False)
--- app/routes.py
+++ app/routes.py
@@ -1,83 +1,24 @@
-from flask import render_template, url_for, flash, redirect, request, Blueprint
-from app import db, bcrypt
-from app.models import User
-from flask_login import login_user, current_user, logout_user, login_required
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length, ValidationError
+from email_validator import validate_email, EmailNotValidError
 
-main = Blueprint('main', __name__)
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[
+        DataRequired(), Length(min=2, max=80)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[
+        DataRequired(), Length(min=6)])
+    submit = SubmitField('Register')
 
-@main.route("/")
-@main.route("/login", methods=['GET', 'POST'])
-def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('main.dashboard'))
-    if request.method == 'POST':
-        email = request.form.get('email')
-        password = request.form.get('password')
-        user = User.query.filter_by(email=email).first()
-        if user and bcrypt.check_password_hash(user.password, password):
-            login_user(user)
-            return redirect(url_for('main.dashboard'))
-        else:
-            flash('Login Unsuccessful. Please check email and password', 'danger')
-    return render_template('login.html')
+    def validate_email(self, email):
+        try:
+            # Validasi email
+            validate_email(email.data)
+        except EmailNotValidError as e:
+            raise ValidationError(str(e))
 
-@main.route("/register", methods=['GET', 'POST'])
-def register():
-    if current_user.is_authenticated:
-        return redirect(url_for('main.dashboard'))
-    if request.method == 'POST':
-        username = request.form.get('username')
-        email = request.form.get('email')
-        password = bcrypt.generate_password_hash(request.form.get('password')).decode('utf-8')
-        user = User(username=username, email=email, password=password)
-        db.session.add(user)
-        db.session.commit()
-        flash('Your account has been created! You are now able to log in', 'success')
-        return redirect(url_for('main.login'))
-    return render_template('register.html')
-
-@main.route("/dashboard")
-@login_required
-def dashboard():
-    users = User.query.all()
-    return render_template('dashboard.html', users=users)
-
-@main.route("/logout")
-def logout():
-    logout_user()
-    return redirect(url_for('main.login'))
-
-@main.route("/add_user", methods=['GET', 'POST'])
-@login_required
-def add_user():
-    if request.method == 'POST':
-        username = request.form.get('username')
-        email = request.form.get('email')
-        password = bcrypt.generate_password_hash(request.form.get('password')).decode('utf-8')
-        user = User(username=username, email=email, password=password)
-        db.session.add(user)
-        db.session.commit()
-        flash('User has been added!', 'success')
-        return redirect(url_for('main.dashboard'))
-    return render_template('add_user.html')
-
-@main.route("/edit_user/<int:user_id>", methods=['GET', 'POST'])
-@login_required
-def edit_user(user_id):
-    user = User.query.get_or_404(user_id)
-    if request.method == 'POST':
-        user.username = request.form.get('username')
-        user.email = request.form.get('email')
-        db.session.commit()
-        flash('User has been updated!', 'success')
-        return redirect(url_for('main.dashboard'))
-    return render_template('edit_user.html', user=user)
-
-@main.route("/delete_user/<int:user_id>", methods=['POST'])
-@login_required
-def delete_user(user_id):
-    user = User.query.get_or_404(user_id)
-    db.session.delete(user)
-    db.session.commit()
-    flash('User has been deleted!', 'success')
-    return redirect(url_for('main.dashboard'))
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
